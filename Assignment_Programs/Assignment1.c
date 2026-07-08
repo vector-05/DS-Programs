e444a349b21756d5a70e8c6d69a77ab9c3845315
Vector05 <mathurraghav23@gmail.com>
2026-07-08 14:25:06 +0000

    Initial Commit

---
+++ Assignment_Programs/Assignment1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void input() {
+    
+}
+
+void display() {
+
+}
+
+void add() {
+
+}
+
+void sub() {
+
+}
+
+void mul() {
+
+}
+
+void tr() {
+
+}
+
+
+int main() {
+
+    // Assignment 1
+    // Program to Input, Display, Add, Subtract, Multiply and Transpose
+    // Number of matrices = 2
+
+    printf("");
+
+    return 0;
+}
