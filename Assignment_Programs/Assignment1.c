d511c3471aae485a701846b85cf711bf6327d6ee
Vector05 <mathurraghav23@gmail.com>
2026-07-08 15:10:48 +0000

    assignment1

--- Assignment_Programs/Assignment1.c
+++ Assignment_Programs/Assignment1.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-void input() {
-    
+int input(char name, int matrix[100][100], int n, int m) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("Enter element [%d][%d] of Matrix %c\n", i, j, name); scanf("%d", &matrix[i][j]);
+        }
+    }
 }
 
 void display() {
@@ -31,7 +35,17 @@ int main() {
     // Program to Input, Display, Add, Subtract, Multiply and Transpose
     // Number of matrices = 2
 
-    printf("");
+    int a_row, a_col, b_row, b_col;
+
+    printf("Enter rows in Matrix A"); scanf("%d\n", &a_row);
+    printf("Enter cols in Matrix A"); scanf("%d\n", &a_col);
+    printf("Enter rows in Matrix B"); scanf("%d\n", &b_row);
+    printf("Enter cols in Matrix B"); scanf("%d\n", &b_col);
+
+    int a[a_row][a_col], b[b_row][b_col];
+
+    input('A', a, a_row, a_col);
+    input('B', b, b_row, b_col);
 
     return 0;
 }
